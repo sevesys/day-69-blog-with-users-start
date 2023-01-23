0219566bddd412468e0d90e7e6f58a8700e10549
Jeffrey Clarke <sevesys@live.com>
2023-01-22 20:42:05 -0500

    correct psycopg2-binary == version. before it was only one = sign remove secret key in comment

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ Base = declarative_base()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-    #'8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
