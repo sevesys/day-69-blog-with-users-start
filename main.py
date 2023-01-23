c780fe5b954feb6bb0c2576bec32099432de6445
Jeffrey Clarke <sevesys@live.com>
2023-01-22 20:31:24 -0500

    Change for using environment variables. add psycopg2-binary (postgresql i think) to requirements and installation.

--- main.py
+++ main.py
@@ -13,16 +13,18 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from urllib.parse import urlparse, urljoin
 from functools import wraps
+import os
 
 Base = declarative_base()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+    #'8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")  # use DATABASE_URL if available (web), else use sqlite;///blog.db (local)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
